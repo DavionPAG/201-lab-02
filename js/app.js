d94a40dab886f9f9ce79c890f0ff810660c96b47
root <root@Vektur.localdomain>
2020-09-30 22:18:14 -0700

    notDoneYet

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var live = prompt('Do I live in seattle?').toLowerCase();
+console.log(live + ' Correctamundo!');
+if (live === 'yes') {
+  alert('Correctamundo!');
+}
+
+
+var futbol = prompt('Do I play futbol?').toLowerCase();
+console.log(futbol + ' I am indeed a practitioner of the worlds\' game.');
+if (futbol === 'yes') {
+  alert('I am indeed a practitioner of the worlds\' game.');
+}
+
+var hike = prompt('Do I like to hike?').toLowerCase();
+console.log(hike + ' Parts unknown is the aim.');
+if (hike === 'yes') {
+  alert('Parts unknown is the aim.');
+}
+
+
+var birds = prompt('Do I like birds?').toLowerCase();
+console.log(birds + ' Freedom!');
+if (birds === 'yes') {
+  alert('Freedom!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
