a75b2cf9390b15d11b76d7fcc33d519ef46cdc2b
root <root@Vektur.localdomain>
2020-10-01 21:04:00 -0700

    lab2Done

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
 'use strict';
+var username = prompt('Greetings! What\'s your name?');
+alert('Welcome ' + username + ', please answer the questions that follow before continuing.');
 
 var live = prompt('Do I live in seattle?').toLowerCase();
-console.log(live + ' Correctamundo!');
+//console.log(live + ' Correctamundo!');
 if (live === 'yes') {
   alert('Correctamundo!');
 }
 
 
 var futbol = prompt('Do I play futbol?').toLowerCase();
-console.log(futbol + ' I am indeed a practitioner of the worlds\' game.');
+//console.log(futbol + ' I am indeed a practitioner of the worlds\' game.');
 if (futbol === 'yes') {
   alert('I am indeed a practitioner of the worlds\' game.');
 }
 
 var hike = prompt('Do I like to hike?').toLowerCase();
-console.log(hike + ' Parts unknown is the aim.');
+//console.log(hike + ' Parts unknown for sure.');
 if (hike === 'yes') {
-  alert('Parts unknown is the aim.');
+  alert('Parts unknown for sure.');
 }
 
-
+var tall = prompt('Am I taller than you?').toLowerCase();
+//console.log('Indeed I am :\)');
+if (tall === 'yes') {
+  alert('Indeed I am :\)');
+}
 var birds = prompt('Do I like birds?').toLowerCase();
-console.log(birds + ' Freedom!');
+//console.log(birds + ' Freedom!');
 if (birds === 'yes') {
   alert('Freedom!');
 }
 
-
-
-
-
-
-
-
-
-
-
-
+alert(username + ', thank your for time. Enjoy!');
